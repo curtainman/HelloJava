50d74eb47efa19d11223b5d8afd5eff0a973dbb4
Alexander Sanderson <asanderson5@huskers.unl.edu>
2024-08-27 11:58:45 -0500

    added sanderson\'s way more important major

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Alex Schwarz");
         System.out.println("Computer Science!");
+        System.out.println("Alexander Sanderson!");
+        System.out.println("Computer Science");
     }
 }
