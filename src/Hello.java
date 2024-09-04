adc62256598b5fcfc71da9d7e81d557f2a8eade1
Alex <54963771+curtainman@users.noreply.github.com>
2024-09-04 18:05:34 -0500

    fix

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,13 @@
+```
+/**
+ * Author: Alex Schwarz
+ *         aschwarz9@huskers.unl.edu
+ * Date: 2024/08/25
+ *
+ * A simple hello world program in Java
+ *
+ */
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Alex Schwarz");
